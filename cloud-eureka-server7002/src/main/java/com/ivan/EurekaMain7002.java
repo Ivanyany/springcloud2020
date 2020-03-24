03c88724b9e253bf8cff3d0677169d8480f9accf
Ivanyany <754339948@qq.com>
2020-03-24 22:45:53 +0800

    集群版eureka-server

--- cloud-eureka-server7002/src/main/java/com/ivan/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/ivan/EurekaMain7002.java
@@ -2,18 +2,18 @@ package com.ivan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Auther: Ivan
- * @Date: 2020/3/23 17:12
+ * @Date: 2020/3/23 19:02
  * @Description:
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer
+public class EurekaMain7002 {
 
     public static void main(String[] args){
-        SpringApplication.run(OrderMain80.class, args);
+        SpringApplication.run(EurekaMain7002.class, args);
     }
 }
